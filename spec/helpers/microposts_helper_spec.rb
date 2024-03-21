e4ee3d9d0f0a9656b921e6be6d5e2e6781b998e0
keizou_nakamura <k-nakamura@ruby-dev.jp>
2024-03-21 15:21:45 +0900

    テストケース追加

--- spec/helpers/microposts_helper_spec.rb
+++ spec/helpers/microposts_helper_spec.rb
@@ -14,10 +14,29 @@ RSpec.describe MicropostsHelper, type: :helper do
       expect(content_for_reply(micropost.content)).to eq "@heyyyy hello!"
     end
 
-    it 'content内に@nameが複数あり、その内一つがユーザーが存在し、もう一つがユーザーが存在しない' do
+    it 'content内に@nameが二つあり、その内一つがユーザーが存在し、もう一つがユーザーが存在しない' do
       user = FactoryBot.create(:user, name: 'hey')
       micropost = user.microposts.create!(content: '@hey hello! and @hi hello!')
       expect(content_for_reply(micropost.content)).to eq "<a href=\"/users/#{user.id}\">@hey</a> hello! and @hi hello!"
+    end
+
+    it 'content内に@nameが三つあり、その内二つがユーザーが存在し、一つがユーザーが存在しない' do
+      user_hey = FactoryBot.create(:user, name: 'hey')
+      user_hi = FactoryBot.create(:user, name: 'hi')
+      micropost = user_hey.microposts.create!(content: '@hey hello! and @hi hello! and @yey!')
+      expect(content_for_reply(micropost.content)).to eq "<a href=\"/users/#{user_hey.id}\">@hey</a> hello! and <a href=\"/users/#{user_hi.id}\">@hi</a> hello! and @yey!"
+    end
+
+    it 'content内の@nameの後ろに.が続く時' do
+      user = FactoryBot.create(:user, name: 'hey')
+      micropost = user.microposts.create!(content: '@hey. hello! and @hi hello!')
+      expect(content_for_reply(micropost.content)).to eq "<a href=\"/users/#{user.id}\">@hey</a>. hello! and @hi hello!"
+    end
+
+    it 'content内の@nameの後ろに!が続く時' do
+      user = FactoryBot.create(:user, name: 'hey')
+      micropost = user.microposts.create!(content: '@hey! hello! and @hi hello!')
+      expect(content_for_reply(micropost.content)).to eq "<a href=\"/users/#{user.id}\">@hey</a>! hello! and @hi hello!"
     end
   end
 end
